d07d01430a7e99db2de790cc62f1bfdd749dcb0c
App Academy Student <student@appacademy.io>
2015-11-23 11:47:36 -0800

    implement user model and controller

--- Gemfile
+++ Gemfile
@@ -32,6 +32,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 gem 'pry-rails'
+gem 'bcrypt'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
@@ -39,6 +40,7 @@ group :development, :test do
   gem 'capybara'
   gem 'factory_girl_rails'
   gem 'rspec-rails'
+  gem 'launchy'
 end
 
 group :development do
---
+++ app/models/user.rb
@@ -0,0 +1,32 @@
+class User < ActiveRecord::Base
+  validates :username, :password_digest, :session_token, presence: true
+  validates :password, length: {minimum: 6, allow_nil: true}
+  attr_reader :password
+  after_intialize :ensure_session_token
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def self.find_by_credentials(username, password)
+    juser = User.find_by(:username, username)
+    return nil unless juser && juser.is_password?(password)
+    juser
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest) == password
+  end
+
+  def reset_session_token!
+    self.session_token = SecureRandom.urlsafe_base64
+    self.save!
+    self.session_token
+  end
+
+  def ensure_session_token
+    self.session_token ||= SecureRandom.urlsafe_base64
+  end
+
+end
